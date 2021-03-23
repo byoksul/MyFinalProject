3f6e7dff67b353d98a2e9284b0a5126d34981f86
byoksul <byoksuul.35@gmail.com>
2021-03-23 22:12:27 +0300

    Güncellemeler yapıldı.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -93,7 +93,7 @@ namespace Business.Concrete
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour ==22 )
             {
                 return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
 
